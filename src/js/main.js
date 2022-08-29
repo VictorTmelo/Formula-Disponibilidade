52a271d14eb9331dec631e97606024e12abae3cc
Victor Torres <victor.tmeloo@gmail.com>
2022-08-29 00:49:33 -0300

    Good Commit

---
+++ src/js/main.js
@@ -0,0 +1,13 @@
+const botaoRodar = document.querySelector("#rodar")
+
+botaoRodar.addEventListener("click", (event) => {
+
+    const n = document.querySelector("#valorN").value
+    const k = document.querySelector("#valorK").value
+    const p = document.querySelector("#valorP").value
+
+    console.log(n)
+    console.log(k)
+    console.log(p)
+
+})
