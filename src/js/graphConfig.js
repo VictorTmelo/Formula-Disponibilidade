42aabc92bd8c3de73458fa505f4b5efe0dacddfe
Victor Torres <victor.tmeloo@gmail.com>
2022-09-02 19:06:00 -0300

    Functional Version

--- src/js/graphConfig.js
+++ src/js/graphConfig.js
@@ -1,34 +1,27 @@
 export const graphConfig = (grafico1, grafico2, grafico3) => {
 
-    const labels = [
-        `${grafico1[0][0]}`, 
-        `${grafico1[1][0]}`,
-        `${grafico1[2][0]}`,
-        `${grafico1[3][0]}`,
-        `${grafico1[4][0]}`,
-        `${grafico1[5][0]}`,
-        `${grafico1[6][0]}`,
-        `${grafico1[7][0]}`,
-        `${grafico1[8][0]}`,
-        `${grafico1[9][0]}`
-    ];
+    const labels = [];
+
+    for(let i = 0; i < 10; i++){
+        labels.push(`${grafico1[i][0]}`)
+    }
     
     const data = {
         labels: labels,
         datasets: [{
-            label: 'Gráfico 1',
-            backgroundColor: 'rgb(255, 99, 132)',
-            borderColor: 'rgb(255, 99, 132)',
+            label: 'K = 1',
+            backgroundColor: 'rgb(58, 235, 52)',
+            borderColor: 'rgb(58, 235, 52)',
             data: [grafico1[0][1], grafico1[1][1], grafico1[2][1], grafico1[3][1], grafico1[4][1], grafico1[5][1], grafico1[6][1], grafico1[7][1], grafico1[8][1], grafico1[9][1]]
         },
         {
-            label: 'Gráfico 2',
-            backgroundColor: 'rgb(255, 99, 132)',
-            borderColor: 'rgb(255, 99, 132)',
+            label: 'K = N / 2',
+            backgroundColor: 'rgb(235, 235, 52)',
+            borderColor: 'rgb(235, 235, 52)',
             data: [grafico2[0][1], grafico2[1][1], grafico2[2][1], grafico2[3][1], grafico2[4][1], grafico2[5][1], grafico2[6][1], grafico2[7][1], grafico2[8][1], grafico2[9][1]]   
         },
         {
-            label: 'Gráfico 3',
+            label: 'K = N',
             backgroundColor: 'rgb(255, 99, 132)',
             borderColor: 'rgb(255, 99, 132)',
             data: [grafico3[0][1], grafico3[1][1], grafico3[2][1], grafico3[3][1], grafico3[4][1], grafico3[5][1], grafico3[6][1], grafico3[7][1], grafico3[8][1], grafico3[9][1]]  
--- src/js/main.js
+++ src/js/main.js
@@ -5,33 +5,60 @@ const botaoRodar = document.querySelector("#rodar")
 botaoRodar.addEventListener("click", (event) => {
 
     const n = document.querySelector("#valorN").value
-    
+
     const p = document.querySelector("#valorP").value
 
     const k1 = 1
     const k2 = n / 2
     const k3 = n
 
-    const grafico1 = graph1(n,k1,p)
-    const grafico2 = graph1(n,k2,p)
-    const grafico3 = graph1(n,k3,p)
+    const grafico1 = graph1(n, k1, p)
+    const grafico2 = graph2(n, k2, p)
+    const grafico3 = graph3(n, k3, p)
 
     graphConfig(grafico1, grafico2, grafico3)
-    
+
 })
 
-function graph1(n, k, p){
+function graph1(n, k, p) {
 
     const array = []
 
-    for (let i = 1; i <= n; i++){
+    for (let i = 1; i <= n; i++) {
 
-        array.push([i, disponibilidade(i,k,p)])
+        array.push([i, disponibilidade(i, 1, p)])
 
     }
-    console.log(array)
     return array
 }
 
+
+function graph2(n, k, p) {
+
+    const array = []
+
+    for (let i = 1; i <= n; i++) {
+
+        if (i % 2 != 0) array.push([i, disponibilidade(i, (Math.floor(i / 2) + 1), p)])
+        if (i % 2 == 0) array.push([i, disponibilidade(i, (Math.floor(i / 2)), p)])
+
+    }
+    
+    return array
+}
+
+function graph3(n, k, p) {
+
+    const array = []
+
+    for (let i = 1; i <= n; i++) {
+
+        array.push([i, disponibilidade(i, i, p)])
+
+
+
+    }
+    return array
+}
 
 
